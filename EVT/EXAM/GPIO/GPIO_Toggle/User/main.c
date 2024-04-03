d927dc1527b5ce082b70fb686624714972f2eb4a
Harkányi Gábor <gabor.harkanyi@yahoo.com>
2024-04-03 17:19:06 +0200

    Adapt GPIO_Toggle to nanoCH32V003 v1.0 board

--- EVT/EXAM/GPIO/GPIO_Toggle/User/main.c
+++ EVT/EXAM/GPIO/GPIO_Toggle/User/main.c
@@ -34,7 +34,7 @@ void GPIO_Toggle_INIT(void)
     GPIO_InitTypeDef GPIO_InitStructure = {0};
 
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE);
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_Init(GPIOD, &GPIO_InitStructure);
@@ -49,8 +49,6 @@ void GPIO_Toggle_INIT(void)
  */
 int main(void)
 {
-    u8 i = 0;
-
     NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1);
     SystemCoreClockUpdate();
     Delay_Init();
@@ -60,7 +58,7 @@ int main(void)
     USART_Printf_Init(115200);
 #endif
     printf("SystemClk:%d\r\n", SystemCoreClock);
-    printf( "ChipID:%08x\r\n", DBGMCU_GetCHIPID() );
+    printf("ChipID:%08x\r\n", DBGMCU_GetCHIPID());
     printf("GPIO Toggle TEST\r\n");
     
     GPIO_Toggle_INIT();
@@ -68,6 +66,6 @@ int main(void)
     while(1)
     {
         Delay_Ms(250);
-        GPIO_WriteBit(GPIOD, GPIO_Pin_0, (i == 0) ? (i = Bit_SET) : (i = Bit_RESET));
+        GPIO_WriteBit(GPIOD, GPIO_Pin_6, !GPIO_ReadOutputDataBit(GPIOD, GPIO_Pin_6));
     }
 }
