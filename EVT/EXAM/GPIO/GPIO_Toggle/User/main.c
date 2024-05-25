3b5e6f55d9717c5a979ae68c0fd44caec65305d1
Harkányi Gábor <gabor.harkanyi@yahoo.com>
2024-05-25 11:30:30 +0200

    Adapt port pin to TIM tests

--- EVT/EXAM/GPIO/GPIO_Toggle/User/main.c
+++ EVT/EXAM/GPIO/GPIO_Toggle/User/main.c
@@ -34,7 +34,7 @@ void GPIO_Toggle_INIT(void)
     GPIO_InitTypeDef GPIO_InitStructure = {0};
 
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE);
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_Init(GPIOD, &GPIO_InitStructure);
@@ -68,6 +68,6 @@ int main(void)
     while(1)
     {
         Delay_Ms(100);
-        GPIO_WriteBit(GPIOD, GPIO_Pin_0, (i == 0) ? (i = Bit_SET) : (i = Bit_RESET));
+        GPIO_WriteBit(GPIOD, GPIO_Pin_4, (i == 0) ? (i = Bit_SET) : (i = Bit_RESET));
     }
 }
