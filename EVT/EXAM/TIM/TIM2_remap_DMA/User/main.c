e9e2dad8c7533983309a7e416e3fed0e4f2e462d
Harkányi Gábor <gabor.harkanyi@yahoo.com>
2024-05-26 16:48:23 +0200

    Port TIM_DMA form TIM1 to TIM2_remap_DMA project
    TODO: PWM period measured 101 ms ...

---
+++ EVT/EXAM/TIM/TIM2_remap_DMA/User/main.c
@@ -0,0 +1,156 @@
+/********************************** (C) COPYRIGHT *******************************
+ * File Name          : main.c
+ * Author             : WCH
+ * Version            : V1.0.0
+ * Date               : 2022/08/08
+ * Description        : Main program body.
+ *********************************************************************************
+ * Copyright (c) 2021 Nanjing Qinheng Microelectronics Co., Ltd.
+ * Attention: This software (modified or not) and binary are used for 
+ * microcontroller manufactured by Nanjing Qinheng Microelectronics.
+ *******************************************************************************/
+
+/*
+ *@Note
+ *Timer DMA routines:
+ *TIM2_CH3 (remap_1: PD2)
+ *This example demonstrates using DMA to output PWM through TIM2_CH3(remap_1: PD2) pin.
+ *
+ */
+/*
+ *  Timer 2 pin mappings by AFIO->PCFR1
+    00  AFIO_PCFR1_TIM2_REMAP_NOREMAP
+        D4      T2CH1ETR
+        D3      T2CH2
+        C0      T2CH3
+        D7      T2CH4  --note: requires disabling nRST in opt
+    01  AFIO_PCFR1_TIM2_REMAP_PARTIALREMAP1
+        C5      T2CH1ETR_
+        C2      T2CH2_
+  --->  D2      T2CH3_
+        C1      T2CH4_
+    10  AFIO_PCFR1_TIM2_REMAP_PARTIALREMAP2
+        C1      T2CH1ETR_
+        D3      T2CH2
+        C0      T2CH3
+        D7      T2CH4  --note: requires disabling nRST in opt
+    11  AFIO_PCFR1_TIM2_REMAP_FULLREMAP
+        C1      T2CH1ETR_
+        C7      T2CH2_
+        D6      T2CH3_
+        D5      T2CH4_
+*/
+
+#include "debug.h"
+
+/* Private variables */
+u16 pbuf[3] = {10, 50, 80};
+
+/*********************************************************************
+ * @fn      TIM2_PWMOut_Init
+ *
+ * @brief   Initializes TIM2 PWM output.
+ *
+ * @param   arr - the period value.
+ *          psc - the prescaler value.
+ *          ccp - the pulse value.
+ *
+ * @return  none
+ */
+void TIM2_PWMOut_Init(u16 arr, u16 psc, u16 ccp)
+{
+    GPIO_InitTypeDef        GPIO_InitStructure = {0};
+    TIM_OCInitTypeDef       TIM_OCInitStructure = {0};
+    TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStructure = {0};
+
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO | RCC_APB2Periph_GPIOD, ENABLE);
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+
+    GPIO_PinRemapConfig(GPIO_PartialRemap1_TIM2, ENABLE);
+
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_30MHz;
+    GPIO_Init(GPIOD, &GPIO_InitStructure);
+
+    TIM_TimeBaseInitStructure.TIM_Period = arr;
+    TIM_TimeBaseInitStructure.TIM_Prescaler = psc;
+    TIM_TimeBaseInitStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+    TIM_TimeBaseInitStructure.TIM_CounterMode = TIM_CounterMode_Up;
+    TIM_TimeBaseInitStructure.TIM_RepetitionCounter = 0;
+    TIM_TimeBaseInit(TIM2, &TIM_TimeBaseInitStructure);
+
+    TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM2;
+    TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+    TIM_OCInitStructure.TIM_Pulse = ccp;
+    TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
+    TIM_OC3Init(TIM2, &TIM_OCInitStructure);
+
+    TIM_OC3PreloadConfig(TIM2, TIM_OCPreload_Disable);
+    TIM_ARRPreloadConfig(TIM2, ENABLE);
+}
+
+/*********************************************************************
+ * @fn      TIM2_DMA_Init
+ *
+ * @brief   Initializes the TIM DMAy Channelx configuration.
+ *
+ * @param   DMA_CHx -
+ *            x can be 1 to 7.
+ *          ppadr - Peripheral base address.
+ *          memadr - Memory base address.
+ *          bufsize - DMA channel buffer size.
+ *
+ * @return  none
+ */
+void TIM2_DMA_Init(DMA_Channel_TypeDef *DMA_CHx, u32 ppadr, u32 memadr, u16 bufsize)
+{
+    DMA_InitTypeDef DMA_InitStructure = {0};
+
+    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
+
+    DMA_DeInit(DMA_CHx);
+    DMA_InitStructure.DMA_PeripheralBaseAddr = ppadr;
+    DMA_InitStructure.DMA_MemoryBaseAddr = memadr;
+    DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralDST;
+    DMA_InitStructure.DMA_BufferSize = bufsize;
+    DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+    DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
+    DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord;
+    DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_HalfWord;
+    DMA_InitStructure.DMA_Mode = DMA_Mode_Circular;
+    DMA_InitStructure.DMA_Priority = DMA_Priority_VeryHigh;
+    DMA_InitStructure.DMA_M2M = DMA_M2M_Disable;
+    DMA_Init(DMA_CHx, &DMA_InitStructure);
+
+    DMA_Cmd(DMA_CHx, ENABLE);
+}
+
+/*********************************************************************
+ * @fn      main
+ *
+ * @brief   Main program.
+ *
+ * @return  none
+ */
+int main(void)
+{
+    SystemCoreClockUpdate();
+    Delay_Init();
+#if (SDI_PRINT == SDI_PR_OPEN)
+    SDI_Printf_Enable();
+#else
+    USART_Printf_Init(115200);
+#endif
+    printf("SystemClk:%d\r\n", SystemCoreClock);
+    printf( "ChipID:%08x\r\n", DBGMCU_GetCHIPID() );
+
+    TIM2_PWMOut_Init(100, 48000 - 1, pbuf[2]);
+    TIM2_DMA_Init(DMA1_Channel2, (u32)&TIM2->CH3CVR, (u32)pbuf, 3);
+
+    TIM_DMACmd(TIM2, TIM_DMA_Update, ENABLE);
+    TIM_Cmd(TIM2, ENABLE);
+    TIM_CtrlPWMOutputs(TIM2, ENABLE);
+
+    while(1);
+}
